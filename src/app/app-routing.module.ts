31866630af805bce424f7ac84da21c0821d0201c
CARLOS ANTONIO ORDOÑEZ BAQUERO <baquerok62@gmail.com>
2023-03-15 19:02:11 -0500

    Se Agrega nuevo enrutamiento para farmaco

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RepuestosComponent } from './Components/repuestos/repuestos.component';
+import { FarmacoComponent } from './Components/farmaco/farmaco.component';
 
 const routes: Routes = [
-  {path:'repuestos',component: RepuestosComponent}
+  {path:'repuestos',component: RepuestosComponent},
+  {path:'farmaco',component: FarmacoComponent}
 ];
 
 @NgModule({
