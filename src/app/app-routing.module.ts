002774f7f7333f87f656f1798e273edd95b1718f
CARLOS ANTONIO ORDOÑEZ BAQUERO <baquerok62@gmail.com>
2023-03-15 19:04:43 -0500

    Nuevo enrutamiento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RepuestosComponent } from './Components/repuestos/repuestos.component';
 import { FarmacoComponent } from './Components/farmaco/farmaco.component';
+import { DrogueriaComponent } from './Components/drogueria/drogueria.component';
 
 const routes: Routes = [
   {path:'repuestos',component: RepuestosComponent},
+  {path:'drogueria',component: DrogueriaComponent},
   {path:'farmaco',component: FarmacoComponent}
 ];
 
